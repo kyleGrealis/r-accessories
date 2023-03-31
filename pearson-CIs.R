980bab8f7d1fc4be38c01f3405191b9951f38675
Kyle Grealis <kylegrealis@icloud.com>
2023-03-31 13:54:19 -0400

    function to calculate Pearson r and 95%CIs

---
+++ pearson-CIs.R
@@ -0,0 +1,28 @@
+# calculate 95%CIs for Pearson's
+#
+# place var1 & var2 in quotes: r_ci(dataset, 'var1', 'var2')
+r_ci <- function(dsn, var1, var2) {
+  # dsn = dataset name
+  # var1 = first variable
+  # var2 = second variable
+  n=nrow(dsn)
+  # calculate Pearson's r and remove NA values
+  r=cor(dsn[var1], dsn[var2], method = 'pearson',
+        use = 'pairwise.complete.obs')
+  # calculate for 95%CIs using Fisher's transformation
+  z=0.5 * (log((1+r)/(1-r)))
+  SEr=sqrt(1/(n-3))
+  limit_1=z-(1.96*SEr)
+  limit_2=z+(1.96*SEr)
+  # back transform to obtain final 95%CIs
+  limit_lower=(exp(2*limit_1)-1)/(exp(2*limit_1)+1)
+  limit_upper=(exp(2*limit_2)-1)/(exp(2*limit_2)+1)
+  # display results in the console
+  print(sprintf('N: %0.0f', n))
+  print(sprintf("Pearson's r: %0.3f", r))
+  print(sprintf('LCL: %0.3f', limit_lower))
+  print(sprintf('UCL: %0.3f', limit_upper))
+}
+
+r_ci(prism, 'catscore', 'sgrq_score')
+
