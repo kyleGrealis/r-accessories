c159ba5b3be601c9ee0eab1d3c205a75d6e72a3a
Kyle Grealis <kylegrealis@icloud.com>
2023-04-11 08:51:04 -0400

    name changed, code is the same

--- randomizedBlock.R
+++ randomizedBlock.R
@@ -9,5 +9,5 @@ a2 = a1[order(a1$block, a1$rand), ]
 a2$arm = rep(c("0", "1"), times = length(block)/2)
 assign = a2[order(a2$envelope), ]
 
-assign = tibble(id = seq(1:44, by = 1), assign$arm)
+assign = tibble(id = seq(1:N, by = 1), assign$arm)
 assign
