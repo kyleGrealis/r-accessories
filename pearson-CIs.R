fd4ea53cd2ebdd08c23d3aac144b01b9219bf9f1
Kyle Grealis <kylegrealis@icloud.com>
2023-06-02 09:42:07 -0400

    option to choose correlation method; more flexible

--- pearson-CIs.R
+++ pearson-CIs.R
@@ -1,13 +1,13 @@
 # calculate 95%CIs for Pearson's
 #
 # place var1 & var2 in quotes: r_ci(dataset, 'var1', 'var2')
-r_ci <- function(dsn, var1, var2) {
+r_ci <- function(dsn, var1, var2, method = 'pearson') {
   # dsn = dataset name
   # var1 = first variable
   # var2 = second variable
   n=nrow(dsn)
   # calculate Pearson's r and remove NA values
-  r=cor(dsn[var1], dsn[var2], method = 'pearson',
+  r=cor(dsn[var1], dsn[var2], method = method,
         use = 'pairwise.complete.obs')
   # calculate for 95%CIs using Fisher's transformation
   z=0.5 * (log((1+r)/(1-r)))
