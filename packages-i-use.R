d8195dcf7d14de2ca35a082a9e107faf6e11ceda
Kyle Grealis <kyleGrealis@icloud.com>
2023-07-17 22:47:05 -0400

    styled script

--- packages-i-use.R
+++ packages-i-use.R
@@ -1,9 +1,9 @@
-if (!require('remotes')) install.packages('remotes')
+if (!require("remotes")) install.packages("remotes")
 
 # for RStudio Theme
 remotes::install_cran(
-  'rsthemes',
-  repos = c(gadenbuie = 'https://gadenbuie.r-universe.dev', getOption('repos'))
+  "rsthemes",
+  repos = c(gadenbuie = "https://gadenbuie.r-universe.dev", getOption("repos"))
 )
 
 rsthemes::install_rsthemes()
@@ -11,11 +11,11 @@ rsthemes::install_rsthemes()
 
 remotes::install_github(
   c(
-    'hadley/emo',
-    'jhelvy/xaringanBuilder',
-    'rstudio/chromote',
-    'clauswilke/colorblindr',
-    'ddsjoberg/gtsummary'
+    "hadley/emo",
+    "jhelvy/xaringanBuilder",
+    "rstudio/chromote",
+    "clauswilke/colorblindr",
+    "ddsjoberg/gtsummary"
   ),
   force = FALSE
 )
@@ -23,99 +23,99 @@ remotes::install_github(
 remotes::install_cran(
   c(
     # Shiny packages:
-    'bs4Dash',
-    'miniUI',
-    'reactlog',
-    'shiny',
-    'shinycssloaders',
-    'shinydashboard',
-    'shinyFeedback',
-    'shinyjs',
-    'shinyMobile',
-    'shiny.semantic',
-    'shinythemes',
-    'shinyWidgets',
-    'waiter',
-    
+    "bs4Dash",
+    "miniUI",
+    "reactlog",
+    "shiny",
+    "shinycssloaders",
+    "shinydashboard",
+    "shinyFeedback",
+    "shinyjs",
+    "shinyMobile",
+    "shiny.semantic",
+    "shinythemes",
+    "shinyWidgets",
+    "waiter",
+
     # webscraping
-    'applicable',
-    'xml2',
-    
+    "applicable",
+    "xml2",
+
     # modeling / stats
-    'tidymodels',
-    'doParallel',
-    'glmnet',
-    'lme4',
-    'ltm',
-    'pwr',
-    'quantmod',
-    'ranger',
-    'vip',
-    
+    "tidymodels",
+    "doParallel",
+    "glmnet",
+    "lme4",
+    "ltm",
+    "pwr",
+    "quantmod",
+    "ranger",
+    "vip",
+
     # plotting / visuals / tables
-    'DiagrammeR',
-    'dotwhisker',
-    'flextable',
-    'ggstatsplot',
-    'ggthemes',
-    'ggtext',
-    'gt',
-    'kable',
-    'kableExtra',
-    'mapproj',
-    'maps',
-    'plotly',
-    'reactable',
-    'reactablefmtr',
-    'rpart.plot',
-    'table1',
-    'textrecipes',
-    'text2vec',
-    'thematic',
-    'vcd',
-    'vcdExtra',
-    'viridis',
-    
+    "DiagrammeR",
+    "dotwhisker",
+    "flextable",
+    "ggstatsplot",
+    "ggthemes",
+    "ggtext",
+    "gt",
+    "kable",
+    "kableExtra",
+    "mapproj",
+    "maps",
+    "plotly",
+    "reactable",
+    "reactablefmtr",
+    "rpart.plot",
+    "table1",
+    "textrecipes",
+    "text2vec",
+    "thematic",
+    "vcd",
+    "vcdExtra",
+    "viridis",
+
     # for package development
-    'available',       
-    'devtools',
-    'gh',
-    'gitcreds',
-    'pak',
-    'pkgbuild',
-    'roxygen2',
-    'testthat',
-    'usethis',
-    
+    "available",
+    "devtools",
+    "gh",
+    "gitcreds",
+    "pak",
+    "pkgbuild",
+    "roxygen2",
+    "testthat",
+    "usethis",
+
     # misc
-    'conflicted',
-    'glue',
-    'haven',
-    'here',
-    'janitor',
-    'jsonlite',
-    'knitr',
-    'lintr',
-    'markdown',
-    'officer',
-    'quarto',
-    'ragg',
-    'redcapAPI',
-    'rio',
-    'rmarkdown',
-    'rsconnect',
-    'rUM',
-    'sjlabelled',
-    'skimr',
-    'styler',
-    'tidyREDCap',
-    'tidyverse',
-    'yaml',
-    
+    "conflicted",
+    "glue",
+    "haven",
+    "here",
+    "janitor",
+    "jsonlite",
+    "knitr",
+    "lintr",
+    "markdown",
+    "officer",
+    "quarto",
+    "ragg",
+    "redcapAPI",
+    "rio",
+    "rmarkdown",
+    "rsconnect",
+    "rUM",
+    "sjlabelled",
+    "skimr",
+    "styler",
+    "tidyREDCap",
+    "tidyverse",
+    "yaml",
+
     # more tidy stuff
-    'tidycensus',
-    'tidycoder',
-    'tidygeocoder'
+    "tidycensus",
+    "tidycoder",
+    "tidygeocoder"
   ),
   # type = 'binary',
   force = FALSE
