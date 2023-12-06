be7c0432d262e1ae532a0254e78979089f8a9883
Kyle Grealis <kyleGrealis@icloud.com>
2023-12-06 14:54:38 -0600

    feat: script to run shiny apps in the background

---
+++ shiny-run.R
@@ -0,0 +1,5 @@
+# this script is used to run the application defined in the project directory
+# see https://github.com/sol-eng/background-jobs/tree/main/shiny-job for more info
+
+options(shiny.autoreload = TRUE)
+shiny::runApp()
